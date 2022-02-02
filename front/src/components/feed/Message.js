c98b73b6aa41173bbe3b1a8b07deed09fe6bdd68
withwalden <74240972+withwalden@users.noreply.github.com>
2022-02-02 19:59:03 +0100

    last updates for a "v1" ready. fetch all mess ok.

--- front/src/components/feed/Message.js
+++ front/src/components/feed/Message.js
@@ -1,29 +1,60 @@
 import React from 'react';
-import GetMess from './GetMess';
 import '../../styles/Message.css';
 
 
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoaded: null, items: [] };
+  }
 
-const Message = () => {
-  return (
+  componentDidMount() {
+    fetch("http://localhost:4200/api/mess")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  } 
+
+  render() {
+    const items = this.state.items;
+    return (
     <div id="mess_container">
-      <div className="messages">
-        <h3>Ceci est un Message</h3>
-        <GetMess />
-      </div>
-      <div className="messages">
-        <h3>Ceci est un Message</h3>
-        <p>vgubj yuvbuy bchgbu cbu iugbuc hbuc cuhr cuzev czeuvbc ezze cezrbuycbuikhoi cziuhgbtv</p>
-      </div>
-      <div className="messages">
-        <h3>Ceci est un Message</h3>
-        <p>vgubj yuvbuy bchgbu cbu iugbuc hbuc cuhr cuzev czeuvbc ezze cezrbuycbuikhoi cziuhgbtv</p>
-      </div>
+      {items.map(item => (
+        <div className="messages" key={item.id}>
+          <div className="messages_top">
+            <h3>username {item.userid}</h3>
+            <p>message numéro {item.id}:</p>
+            <p>{item.content}</p>
+          </div>
+          <div className="message_bottom">
+            <p>comments</p>
+            <p>likes</p>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+    );
+  }
 }
 
 
-
-
 export default Message;
+
+
+
+
+//error non gêré
+//const isLoaded = this.state.items === null ? true : false;
+//const name = isLoaded ? 'Loading...' : this.state.messData.id;
